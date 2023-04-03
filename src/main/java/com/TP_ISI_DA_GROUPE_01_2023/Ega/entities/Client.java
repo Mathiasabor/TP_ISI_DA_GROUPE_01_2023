795e68e8899bd7ef379dbe766475807908e9e34e
sealmee <jamesken9395@gmail.com>
2023-04-03 11:23:47 +0000

    Entities

--- src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/entities/Client.java
+++ src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/entities/Client.java
@@ -18,6 +18,83 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Client {
 
+@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long id;
+
+	    private String nom;
+	    
+		private String prenom;
+	    private LocalDate dateNaissance;
+	    private String sexe;
+	    private String adresse;
+	    
+	    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+	    private List<Compte> comptes;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNom() {
+			return nom;
+		}
+
+		public void setNom(String nom) {
+			this.nom = nom;
+		}
+
+		public String getPrenom() {
+			return prenom;
+		}
+
+		public void setPrenom(String prenom) {
+			this.prenom = prenom;
+		}
+
+		public LocalDate getDateNaissance() {
+			return dateNaissance;
+		}
+
+		public void setDateNaissance(LocalDate dateNaissance) {
+			this.dateNaissance = dateNaissance;
+		}
+
+		public String getSexe() {
+			return sexe;
+		}
+
+		public void setSexe(String sexe) {
+			this.sexe = sexe;
+		}
+
+		public String getAdresse() {
+			return adresse;
+		}
+
+		public void setAdresse(String adresse) {
+			this.adresse = adresse;
+		}
+
+		public List<Compte> getComptes() {
+			return comptes;
+		}
+
+		public void setComptes(List<Compte> comptes) {
+			this.comptes = comptes;
+		}
+		
+		public Client AddComptes(Client client, Compte compte)
+		{
+			client.comptes.add(compte);
+			return client;
+		}
+	    
+
 
 	    
 }
--- src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/entities/Compte.java
+++ src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/entities/Compte.java
@@ -20,7 +20,65 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Compte {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String numero;
+    private String type;
+    private LocalDate dateCreation;
+    private double solde;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id")
+    private Client client;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDate getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(LocalDate dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 		
 
 }
