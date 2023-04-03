b87ddf6df8430968005fca5b228ae35fc137d840
Rhamone <rhamonkota@gmail.com>
2023-04-03 10:32:11 +0000

    les services client

--- src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/services/ClientService.java
+++ src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/services/ClientService.java
@@ -10,4 +10,13 @@ import com.TP_ISI_DA_GROUPE_01_2023.Ega.entities.Compte;
 @Service
 public interface ClientService {
 
+    public List<Client> showClient();
+	public Client saveClient(Client client);
+	public Client getOneClient(Long id);
+	public void deleteClient(Long id);
+	public void AttribCompte(Client client,List<Compte> compte);
+	
+	//fonction pour la creation du compte
+	public String genererNumeroCompte();
+
 }
--- src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/services/ClientServiceImpl.java
+++ src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/services/ClientServiceImpl.java
@@ -14,7 +14,60 @@ import com.TP_ISI_DA_GROUPE_01_2023.Ega.repositories.ClientRepository;
 @Service
 public class ClientServiceImpl implements ClientService {
 	
+    @Autowired
+	private ClientRepository clientRepo;
 
+	@Override
+	public List<Client> showClient() {
+		
+		return clientRepo.findAll();
+	}
+
+	@Override
+	public Client saveClient(Client client) {
+		
+		return clientRepo.save(client);
+	}
+
+	@Override
+	public Client getOneClient(Long id) {
+		
+		return clientRepo.findById(id).get();
+	}
+
+	@Override
+	public void deleteClient(Long id) {
+		clientRepo.deleteById(id);
+
+	}
 	
+	
+
+	@Override
+	public String genererNumeroCompte() {
+		// Générer une chaîne aléatoire de 5 caractères majuscules alphanumériques
+	    String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	    StringBuilder sb = new StringBuilder();
+	    Random random = new Random();
+	    for (int i = 0; i < 5; i++) {
+	        sb.append(characters.charAt(random.nextInt(characters.length())));
+	    }
+	    String numeroCompte = sb.toString();
+	    
+	    // Ajouter l'année actuelle au numéro de compte généré
+	    LocalDateTime date = LocalDateTime.now();
+	    int annee = date.getYear();
+	    numeroCompte += annee;
+	    
+	    return numeroCompte;
+	}
+
+	@Override
+	public void AttribCompte(Client client, List<Compte> compte) {
+		
+		client.setComptes(compte);
+	}
+	
+
 
 }
