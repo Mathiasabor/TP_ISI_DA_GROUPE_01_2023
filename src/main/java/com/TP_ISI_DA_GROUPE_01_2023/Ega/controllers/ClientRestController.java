b8df62735dd11b9f690b5cc9e606ef2a9b601bfa
Mathiasabor <ahomagi0@gmail.com>
2023-04-03 10:13:37 +0000

    controllers

--- src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/controllers/ClientRestController.java
+++ src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/controllers/ClientRestController.java
@@ -18,6 +18,51 @@ import com.TP_ISI_DA_GROUPE_01_2023.Ega.services.CompteService;
 
 @RestController
 public class ClientRestController {
-	
+
+    @Autowired
+    private ClientService clientServ;
+
+    @Autowired
+    private CompteService compteServ;
+    @GetMapping("/clients")
+    public List<Client> listeClient()
+    {
+        return clientServ.showClient();
+    }
+
+    @GetMapping("/clients/{id}")
+    public Client getClient(@PathVariable Long id)
+    {
+        return clientServ.getOneClient(id);
+    }
+
+    @PostMapping("/clients")
+    public Client saveClient(@RequestBody Client client)
+    {
+        return clientServ.saveClient(client);
+    }
+
+    @PutMapping("/client/{id}")
+    public Client AddCompte(Long id,@RequestBody Client client)
+    {
+        Compte cmpt = compteServ.getOneCompte(id);
+        client = client.AddComptes(client,cmpt);
+        return clientServ.saveClient(client);
+    }
+
+    @PutMapping("/clients/{id}")
+    public Client updateClient(@PathVariable Long id, Client client)
+    {
+        client.setId(id);
+        return clientServ.saveClient(client);
+    }
+
+    @DeleteMapping("/client/{id}")
+    public void deleteClient(@PathVariable Long id)
+    {
+        clientServ.deleteClient(id);
+    }
+
+
 
 }
--- src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/controllers/CompteRestController.java
+++ src/main/java/com/TP_ISI_DA_GROUPE_01_2023/Ega/controllers/CompteRestController.java
@@ -18,4 +18,70 @@ import com.TP_ISI_DA_GROUPE_01_2023.Ega.services.CompteService;
 public class CompteRestController {
 
 
+    @Autowired
+    private CompteService compteServ;
+
+
+
+
+    @GetMapping("/comptes")
+    public List<Compte> listeCompte()
+    {
+        return compteServ.showCompte();
+    }
+
+    @GetMapping("/comptes/{id}")
+    public Compte getCompte(@PathVariable Long id)
+    {
+        return compteServ.getOneCompte(id);
+    }
+
+    @PostMapping("/comptes")
+    public Compte saveCompte(@RequestBody Compte compte)
+    {
+        return compteServ.saveCompte(compte);
+    }
+
+    @PutMapping("/comptes/{id}")
+    public Compte updateCompte(@PathVariable Long id, Compte compte)
+    {
+        compte.setId(id);
+        return compteServ.saveCompte(compte);
+    }
+
+    @DeleteMapping("/comptes/{id}")
+    public void deleteCompte(@PathVariable Long id)
+    {
+        compteServ.deleteCompte(id);
+    }
+
+    @PutMapping("retirer/{id}/{solde}")
+    public Compte retirer(@PathVariable Long id, @PathVariable Double solde)
+    {
+        Compte cpte = compteServ.RetirerSolde(id, solde);
+        return compteServ.saveCompte(cpte);
+    }
+
+    @PutMapping("virer/{id}/{montant}/{id1}")
+
+    public Compte virer(@PathVariable Long id, @PathVariable Double montant, @PathVariable Long id1)
+    {
+        Compte cpte = compteServ.Virement(id,montant,id1);
+        return compteServ.saveCompte(cpte);
+    }
+    @PutMapping("ajouter/{id}/{solde}")
+    public Compte Ajouter(@PathVariable Long id, @PathVariable Double solde)
+    {
+        Compte cpte = compteServ.AddSolde(id, solde);
+        return compteServ.saveCompte(cpte);
+    }
+
+    @PostMapping("creer/{Type}")
+    public Compte creerCompte(@PathVariable String Type)
+    {
+        Compte cpte = compteServ.CreateCompte(Type);
+
+        return compteServ.saveCompte(cpte);
+    }
+
 }
